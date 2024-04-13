bafb4045ad61aa7537c4814223ee19693b246705
Rasel Rana Earshad <raselranaearshad@gmail.com>
2024-04-13 21:09:55 +0600

    remove greeting and add display method

--- hello.java
+++ hello.java
@@ -2,10 +2,10 @@ public class hello {
   public static void main (String[] args){
     System.out.println("Vs Code is awesome.")
   
-    greeting("Git");
+    display();
   }
 
-  public static void greeting(String name) {
-    System.out.println("Bye Bye" + name);
+  public static void display(){
+    System.out.println("Hello Hello");
   }
 }
